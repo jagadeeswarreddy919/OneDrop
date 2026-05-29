7cf6fccc87e663618468dc7045042e2db5f339d4
jagadeeswarreddy919 <55675402+jagadeeswarreddy919@users.noreply.github.com>
2026-05-29 14:33:52 +0530

    feat: increase express request payload limit to 50mb

--- raktsetu/server/src/index.js
+++ raktsetu/server/src/index.js
@@ -40,7 +40,8 @@ app.use(cors({
 }));
 
 // Express parser & static upload routes
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // API Rate Limiter
